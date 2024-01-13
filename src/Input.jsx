83583edf8a787ca45020b9e2cf2f63c8038f7343
Elena Korchuganova <korchel21@gmail.com>
2024-01-13 23:18:15 +0300

    validation fixed

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable functional/no-expression-statements */
 import { React } from 'react';
 import {
   Formik, Form, Field, ErrorMessage,
@@ -10,47 +11,58 @@ const Input = () => {
   return (
     <Formik
       validationSchema={validationSchema}
+      validateOnBlur={false}
+      validateOnChange={false}
       initialValues={{
         day: '',
         month: '',
         year: '',
       }}
-      onSubmit={() => {
-        
+      onSubmit={(values) => {
+        const { day, month, year } = values;
+        console.log(new Date(year, month - 1, day));
       }}
     >
       {() => (
-        <Form className="form">
-          <label htmlFor="day" className="input-block label-text">
-            d a y
-            <Field
-              id="day"
-              name="day"
-              type="number"
-              className="field"
-            />
-            <ErrorMessage name="day" component="p" />
-          </label>
-          <label htmlFor="month" className="input-block label-text">
-            m o n t h
-            <Field
-              id="month"
-              name="month"
-              type="number"
-              className="field"
-            />
-            <ErrorMessage name="month" component="p" />
-          </label>
-          <label htmlFor="year" className="input-block label-text">
-            y e a r
-            <Field
-              id="year"
-              name="year"
-              type="number"
-              className="field"
-            />
-            <ErrorMessage name="year" component="p" />
-          </label>
+        <Form>
+          <div className="form">
+            <label htmlFor="day" className="input-block label-text">
+              d a y
+              <Field
+                id="day"
+                name="day"
+                type="number"
+                className="field"
+              />
+              <ErrorMessage name="day" component="p" />
+            </label>
+            <label htmlFor="month" className="input-block label-text">
+              m o n t h
+              <Field
+                id="month"
+                name="month"
+                type="number"
+                className="field"
+              />
+              <ErrorMessage name="month" component="p" />
+            </label>
+            <label htmlFor="year" className="input-block label-text">
+              y e a r
+              <Field
+                id="year"
+                name="year"
+                type="number"
+                className="field"
+              />
+              <ErrorMessage name="year" component="p" />
+            </label>
+          </div>
+          <div className="btn-group">
+            <hr />
+            <button type="submit" className="btn">
+              <span>Submit</span>
+            </button>
+          </div>
         </Form>
       )}
     </Formik>
--- src/validationSchema.js
+++ src/validationSchema.js
@@ -1,15 +1,29 @@
+/* eslint-disable functional/no-expression-statements */
 import * as Yup from 'yup';
+
+const isValidDate = (year, month, day) => {
+  const date = new Date(year, month, day);
+  if (date.getFullYear() === year && date.getMonth() === month && date.getDate() === day) {
+    return true;
+  }
+  return false;
+};
 
 export default Yup.object().shape({
   day: Yup.number()
+    .required('This field is required')
     .min(1, 'Must be a valid day')
     .max(31, 'Must be a valid day')
-    .required(),
+    .test(
+      'date is valid',
+      'Must be a valid date',
+      (day, context) => isValidDate(context.parent.year, context.parent.month - 1, day),
+    ),
   month: Yup.number()
+    .required('This field is required')
     .min(1, 'Must be a valid month')
-    .max(12, 'Must be a valid month')
-    .required(),
+    .max(12, 'Must be a valid month'),
   year: Yup.number()
-    .max(new Date().getFullYear(), 'Must be in the past')
-    .required(),
+    .required('This field is required')
+    .max(new Date().getFullYear(), 'Must be in the past'),
 });
